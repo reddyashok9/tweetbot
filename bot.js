18861dfb476c67e59a5a59b3b03239f42793c74c
Ashok <reddyashok9@gmail.com>
2019-04-24 10:38:36 +0530

    init

--- bot.js
+++ bot.js
@@ -19,41 +19,6 @@ const sendDirectMessage = function () {
             db.getUniqueTweets(data.statuses).then((tweets) => {
                 const content = helpers.composeContent(tweets)
                 db.recordUniqueTweets(tweets)
-
-                Twitter.get('followers/ids', {
-                    user_id: 1,
-                    screen_name: 'ashokreddykl'
-                }, (err, response) => {
-                    if (err) {
-                        console.log('Something went wrong while getting followers. Error: ', err)
-                    }
-
-                    if (response) {
-                        response.ids.forEach((id) => {
-                            Twitter.post('direct_messages/events/new', {
-                                event: {
-                                    'type': 'message_create',
-                                    'message_create': {
-                                        'target': {
-                                            'recipient_id': id
-                                        },
-                                        'message_data': {
-                                            'text': content,
-                                        }
-                                    }
-                                }
-                            }, (err, response) => {
-                                if (err) {
-                                    console.log('Something went wrong while sending message. Error: ', err)
-                                } else {
-                                    if (response) {
-                                        console.log('Message sent. Response: ', response)
-                                    }
-                                }
-                            })
-                        })
-                    }
-                })
             })
         } else {
             console.log('Something went wrong while searching.')
